4b63365aa7acd54c3ef20d4c36ce34d1bab81ad1
Melek Akkoyunlu <melekakkoyunlu3@gmail.com>
2024-01-30 18:17:52 +0300

    syntax fixed

--- branch.cs
+++ branch.cs
@@ -8,8 +8,8 @@ namespace GitDemo
 {
     internal class branch
     {
-        public string Name { get; set; };
-        public string Description { get; set; };
-        public string Author { get; set; };
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
     }
 }
