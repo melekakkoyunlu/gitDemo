c5c201b933ddb52118a11f75a185fe4cfced1cee
Melek Akkoyunlu <melekakkoyunlu3@gmail.com>
2024-01-30 17:59:26 +0300

    TR file added

---
+++ ProgramTr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class ProgramTr
+    {
+        Console.WriteLine("Merhaba Dunya!");
+        Console.ReadLine();
+    }
+}
